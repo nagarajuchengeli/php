f0377bd2e20cc8b7620925d68263477ce060207c
nagarajuchengeli <nagaraju.chengeli@callhealth.co.in>
2016-04-07 16:17:44 +0530

    V1.2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-echo "This is First GitHub Upload V2";
-echo " This Web site is updating please be patient till the next release";
+echo "	This is First GitHub Upload V2.
+		This Web site is updating please be patient till the next release
+		This site is been renewed";
 ?>
